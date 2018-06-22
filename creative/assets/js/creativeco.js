f4030d92dd3a07c4733c2f6fd97239a132d64d06
Elolo Blaise <39464647+Kweyku@users.noreply.github.com>
2018-06-22 13:08:45 +0000

    startup landing page

---
+++ creative/assets/js/creativeco.js
@@ -0,0 +1,21 @@
+/*
+var firstName=prompt("What is your first name");
+var lastName=prompt("What is your last name");
+var userAge=prompt("What is your age");
+
+alert("Your name is " + firstName +  lastName);
+
+console.log("Your name is " + firstName +  lastName);
+console.log("You are " + userAge);*/
+
+/*
+if (100 > 50) {
+    var outcome = "perfecto";
+}**/
+
+if ("learningJava" === "sexy") {
+    var outcome = "this is true";
+} else {
+    var outcome = "not possible";
+}
+
